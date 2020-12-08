a5b1428e6a81b357b1594d122da7112067c81e33
martLeveMart <mart.leveque69@gmail.com>
2020-12-08 13:55:38 +0100

    Composant Player V1 avant retouche graphique

--- fellow-ship/src/components/objects/Player.js
+++ fellow-ship/src/components/objects/Player.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {cloneElement, useState} from "react";
 
 const Player = ({ id, username, firstname, lastname, rate, picture, description, exp, role, classe, niveau}) => {
+	const [visuDetail, setVisuDetail] = useState(false);
+	const [select, setSelect] = useState(null);
+
+	const toggleDetail = () => setVisuDetail(!visuDetail);
+
+	const renderDetail = () => (
+		<div className="player__detail">
+			<div className="player__detail__classe">{ `${classe} niveau ${niveau}`}</div>
+			<div className="player__detail__classe">{ `${description}`}</div> {/*a remplacer par le nombre de seance*/}
+		</div>
+	);
+
 	return (
-		<div className="player">
+		<div className="player" onDoubleClick={toggleDetail}>
 			<div className="player__picture">
 				{/*ajouter la photo*/}
 			</div>
@@ -10,11 +22,10 @@ const Player = ({ id, username, firstname, lastname, rate, picture, description,
 				<div className="player__info__username">{!role && `${username}`}</div>
 				<div className="player__info__username">{!!role && "Maître du Jeu"}</div>
 				<div className="player__info__name">{`${firstname} ${lastname}`}</div>
-				<div className="player__info__classe">{!role && `${classe}`}</div>
+				<div className="player__info__classe">{!role && !visuDetail && `${classe}`}</div>
+				<div className="player__info__experience">{!!visuDetail && `${exp} xp étoiles: ${rate}`}</div>
 			</div>
-			<div className="player__detail">
-				{/*mettre le detail au click*/}
-			</div>
+			{!!visuDetail && renderDetail()}
 		</div>
 	);
 };
